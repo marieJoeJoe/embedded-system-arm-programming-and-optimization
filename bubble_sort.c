f504f7b409f21c8fc9aa57e8176bc93a30d7fd98
marie <marieqoeqoe@gmail.com>
2021-08-16 08:09:58 +0800

    bubble sort asm

---
+++ bubble_sort.c
@@ -0,0 +1,23 @@
+#define N 32768
+
+int data[N];
+
+
+int main()
+{
+  int i,j,temp;
+
+  for ( i = 0; i < (N-1); i++)
+  for ( j = 0; j < (N-1); j++)
+  if(data[j]>data[i]){
+      temp = data[j];
+
+      data[j]=data[j+1];
+
+      data[j+1]=temp;
+
+  }
+  
+  return 1;
+
+}
